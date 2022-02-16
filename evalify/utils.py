f5d62ff29af3703cd6dd9510d5974f2a68353275
ma7555 <ma7moud_alaa@hotmail.com>
2022-02-16 03:15:13 +0200

    lint

--- evalify/utils.py
+++ evalify/utils.py
@@ -1,5 +1,6 @@
-import psutil
 import numpy as np
+import psutil
+
 
 def calc_available_memory():
     mem = psutil.virtual_memory()
@@ -8,15 +9,15 @@ def calc_available_memory():
 
 
 def keep_to_max_rows(embs, available_mem):
-    '''
+    """
     Calculate maximum rows to fetch per split without going out of memory.
-    We will need 3 big arrays to be held in memory (A, B, A*B) 
-    '''
+    We will need 3 big arrays to be held in memory (A, B, A*B)
+    """
     if available_mem > 2e9:
         max_total_rows = np.floor(available_mem - 1e9 / (embs[0].nbytes))
-        max_rows_per_side = int(max_total_rows/3)
+        max_rows_per_side = int(max_total_rows / 3)
     else:
         max_total_rows = np.floor(available_mem / (embs[0].nbytes))
-        max_rows_per_side = int(max_total_rows/5)
+        max_rows_per_side = int(max_total_rows / 5)
 
     return max_rows_per_side
