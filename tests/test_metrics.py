fce04a75ba6f1d66fea33f0b695483f6a5338dc6
ma7555 <ma7moud_alaa@hotmail.com>
2022-02-17 16:20:25 +0200

    properly test metrics against scipy implemntations

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -4,6 +4,7 @@
 import os
 import pathlib
 import sys
+from scipy.spatial import distance
 
 sys.path.append(os.path.join(pathlib.Path(__file__).parent.parent, "evalify"))
 import unittest
@@ -18,34 +19,58 @@ class TestEvalify(unittest.TestCase):
 
     def setUp(self):
         """Set up test fixtures, if any."""
-
-        self.embs = np.arange(12).reshape(4, 3)
-        self.ix_for_metrics = range(4)
+        rng = np.random.default_rng(555)
+        self.nphotos = 500
+        self.emb_size = 8
+        self.size = 100
+        self.embs = rng.random((self.nphotos, self.emb_size), dtype=np.float32)
         self.norms = np.linalg.norm(self.embs, axis=1)
+        self.ix = rng.integers(self.nphotos, size=self.size)
+        self.iy = rng.integers(self.nphotos, size=self.size)
 
     def tearDown(self):
         """Tear down test fixtures, if any."""
 
     def test_cosine_similarity(self):
         """Test cosine_similarity"""
-        result = metrics.cosine_similarity(
-            self.embs, self.ix_for_metrics, self.ix_for_metrics, self.norms
+        result = metrics.cosine_similarity(self.embs, self.ix, self.iy, self.norms)
+        result_2 = 1 - np.array(
+            [
+                distance.cosine(self.embs[ix], self.embs[iy])
+                for (ix, iy) in zip(self.ix, self.iy)
+            ]
         )
-        self.assertEqual(result.shape, (len(self.ix_for_metrics),))
-        self.assertAlmostEqual(result.sum(), len(self.ix_for_metrics))
+        self.assertEqual(result.shape, (self.size,))
+        self.assertTrue(np.allclose(result, result_2))
 
     def test_euclidean_distance(self):
         """Test euclidean_distance"""
-        result = metrics.euclidean_distance(
-            self.embs, self.ix_for_metrics, self.ix_for_metrics
+        result = metrics.euclidean_distance(self.embs, self.ix, self.iy)
+        result_2 = np.array(
+            [
+                distance.euclidean(self.embs[ix], self.embs[iy])
+                for (ix, iy) in zip(self.ix, self.iy)
+            ]
         )
-        self.assertEqual(result.shape, (len(self.ix_for_metrics),))
-        self.assertAlmostEqual(result.sum(), 0)
+        self.assertEqual(result.shape, (self.size,))
+        self.assertTrue(np.allclose(result, result_2))
 
     def test_euclidean_distance_l2(self):
         """Test euclidean_distance"""
-        result = metrics.euclidean_distance(
-            self.embs, self.ix_for_metrics, self.ix_for_metrics, self.norms
+        result = metrics.euclidean_distance(self.embs, self.ix, self.iy, self.norms)
+        result_2 = np.array(
+            [
+                distance.euclidean(
+                    self.embs[ix] / np.sqrt(np.sum(self.embs[ix] ** 2)),
+                    self.embs[iy] / np.sqrt(np.sum(self.embs[iy] ** 2)),
+                )
+                for (ix, iy) in zip(self.ix, self.iy)
+            ]
         )
-        self.assertEqual(result.shape, (len(self.ix_for_metrics),))
-        self.assertAlmostEqual(result.sum(), 0)
+
+        self.assertEqual(result.shape, (len(self.ix),))
+        self.assertTrue(np.allclose(result, result_2))
+
+    def test_get_norm(self):
+        """Test get_norm"""
+        self.assertTrue(np.allclose(self.norms, metrics._get_norms(self.embs)))
