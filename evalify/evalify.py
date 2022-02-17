f7c228446973864aa632c51dba6cadac17db61a9
ma7555 <ma7moud_alaa@hotmail.com>
2022-02-17 04:54:21 +0200

    this should be working

--- evalify/evalify.py
+++ evalify/evalify.py
@@ -17,6 +17,7 @@ def create_experiment(
     different_class_samples: str = "minimal",
     nsplits: Union[str, int] = "best",
     shuffle: bool = False,
+    seed: int = None,
     return_embeddings: bool = False,
 ):
     """Creates an experiment for face verification
@@ -26,27 +27,37 @@ def create_experiment(
         y: Targets for X as integers
         metric: metric used for comparing embeddings distance
         same_class_samples:
-            - 'full': Sampling all possible pairs within each class
-            -  int: Sampling specify number of pairs for
-                every class. If the provided number is greater than
-                the achievable for the class, the maximum possible
-                combinations are used.
+            For creating positive examples.
+            - 'full': Samples all possible images within each class to create all
+                all possible positive pairs.
+            -  int: Samples specific number of images for every class to create
+                nC2 pairs where n is passed integer. If the provided number is greater
+                than the achievable for the class, the maximum possible combinations
+                are used.
         different_class_samples:
-            - 'full': Samples all possible pairs within different classes.
-                This can grow exponentially as the number of images increase.
+            For creating negative samples.
+            - 'full': Samples one image from every class with all possible pairs
+                of different classes. This can grow exponentially as the number
+                of images increase. (N, M) = (1, "full")
             - 'minimal': Samples one image from every class with one image of
-                all other classes. (Default)
-            - int: Samples one image from every class with X images of
-                every other class. If the provided number is greater than
+                all other classes. (N, M) = (1, 1). (Default)
+            - int: Samples one image from every class with provided number of
+                images of every other class. If the provided number is greater than
                 the achievable for the class, the maximum possible combinations
                 are used.
+            - tuple or list: (N, M) Samples N images from every class with M images of
+                every other class. If either is greater than the achievable, the
+                maximum possible combinations are used.(N, M) can also be
+                ('full', 'full') but this will calculate all possible combinations
+                between all posibile negative samples. If the dataset is not small this
+                will probably result in an extremely large array!.
+
         nsplits:
-            - 'best': Let the program decide based on available memory such
-                that every split will fit into the available memory. (Default)
+            - 'best': Let the program decide based on available memory such that every
+                split will fit into the available memory. (Default)
             - int: Manually decide the number of splits.
-        shuffle: Whether to shuffle the returned experiment dataframe.
-            Default: False.
-        return_embeddings: Whether to return the embeddings instead of indexes.
+        shuffle: Whether to shuffle the returned experiment dataframe. Default: False.
+        return_embeddings: Whether to return the embeddings instead of indexes. 
             Default: False
 
     Returns:
@@ -57,17 +68,33 @@ def create_experiment(
     """
     if same_class_samples != "full" and not isinstance(same_class_samples, int):
         raise ValueError(
-            '`same_class_samples` argument must be one of "full" or an integer '
+            "`same_class_samples` argument must be one of 'full' or an integer "
             f"Received: same_class_samples={same_class_samples}"
         )
-    if different_class_samples not in ("full", "minimal") and not isinstance(
-        different_class_samples, int
-    ):
-        raise ValueError(
-            '`different_class_samples` argument must be one of "full", "minimal" '
-            "or an integer "
-            f"Received: different_class_samples={different_class_samples}."
-        )
+    if different_class_samples not in ("full", "minimal"):
+        if not isinstance(different_class_samples, (int, list, tuple)):
+            raise ValueError(
+                "`different_class_samples` argument must be one of 'full', 'minimal', "
+                "an integer, a list or tuple of integers or keyword 'full'."
+                f"Received: different_class_samples={different_class_samples}."
+            )
+        elif isinstance(different_class_samples, (list, tuple)):
+            if (
+                not (
+                    all(
+                        isinstance(i, int) or i == "full"
+                        for i in different_class_samples
+                    )
+                )
+                or (len(different_class_samples)) != 2
+            ):
+                raise ValueError(
+                    "When passing `different_class_samples` as a tuple or list, "
+                    "elements must be exactly two of integer type or keyword 'full' "
+                    f"(N, M). "
+                    "Received: different_class_samples={different_class_samples}."
+                )
+
     if nsplits != "best" and not isinstance(nsplits, int):
         raise ValueError(
             '`nsplits` argument must be either "best" or of type integer '
@@ -82,22 +109,45 @@ def create_experiment(
     all_targets = np.unique(y)
     all_pairs = list()
     metric_fn = metrics_caller.get(metric)
-
+    rng = np.random.default_rng(seed)
+    already_added = set()
     for target in all_targets:
-        same_ixs = np.argwhere(y == target).ravel()
+        same_ixs_full = np.argwhere(y == target).ravel()
+        if isinstance(same_class_samples, int):
+            same_class_samples = min(len(same_ixs_full), same_class_samples)
+            same_ixs = rng.choice(same_ixs_full, same_class_samples)
+        elif same_class_samples == "full":
+            same_ixs = same_ixs_full
         same_pairs = itertools.combinations(same_ixs, 2)
-        same_pairs = [(a, b, target, target, 1) for a, b in same_pairs]
-        different_ixs = np.argwhere(y != target).ravel()
+        same_pairs_expanded = [(a, b, target, target, 1) for a, b in same_pairs]
 
+        different_ixs = np.argwhere(y != target).ravel()
         df = pd.DataFrame(data={"ix": different_ixs, "target": y[different_ixs]})
 
-        if different_class_samples == "minimal":
-            df = df.sample(frac=1).drop_duplicates(subset=["target"])
+        df = df.sample(frac=1, random_state=seed)
+        if different_class_samples in ["full", "minimal"] or isinstance(
+            different_class_samples, int
+        ):
+            N = 1
+            if different_class_samples == "minimal":
+                df = df.drop_duplicates(subset=["target"])
+        else:
+            N, M = different_class_samples
+            N = len(same_ixs_full) if N == "full" else min(N, len(same_ixs_full))
+            if M != "full":
+                df = df.groupby("target").apply(lambda x: x[:M]).droplevel(0)
 
         different_ixs = df.ix.to_numpy()
-        different_pairs = itertools.product([np.random.choice(same_ixs)], different_ixs)
-        different_pairs = [(a, b, target, y[b], 1) for a, b in different_pairs]
-        all_pairs += same_pairs + different_pairs
+
+        different_pairs = itertools.product(
+            rng.choice(same_ixs_full, N, replace=False), different_ixs
+        )
+        different_pairs_expanded = []
+        for a, b in different_pairs:
+            if (a, b) not in already_added:
+                different_pairs_expanded.append((a, b, target, y[b], 0))
+                already_added.update(((a, b), (b, a)))
+        all_pairs += same_pairs_expanded + different_pairs_expanded
 
     one_to_one_df = pd.DataFrame(
         data=all_pairs, columns=["img_a", "img_b", "target_a", "target_b", "target"]
--- tests/test_evalify.py
+++ tests/test_evalify.py
@@ -4,6 +4,7 @@
 import os
 import pathlib
 import sys
+from scipy.special import comb
 
 sys.path.append(os.path.join(pathlib.Path(__file__).parent.parent, "evalify"))
 import unittest
@@ -18,11 +19,12 @@ class TestEvalify(unittest.TestCase):
 
     def setUp(self):
         """Set up test fixtures, if any."""
-        self.nphotos = 5000
-        self.emb_size = 256
-        self.nclasses = 125
-        self.embs = np.random.rand(self.nphotos, self.emb_size)
-        self.targets = np.random.randint(self.nclasses, size=self.nphotos)
+        rng = np.random.default_rng(555)
+        self.nphotos = 500
+        self.emb_size = 8
+        self.nclasses = 10
+        self.embs = rng.random((self.nphotos, self.emb_size), dtype=np.float32)
+        self.targets = rng.integers(self.nclasses, size=self.nphotos)
 
     def tearDown(self):
         """Tear down test fixtures, if any."""
@@ -32,7 +34,11 @@ class TestEvalify(unittest.TestCase):
         df = evalify.create_experiment(
             self.embs, self.targets, metric="euclidean_distance"
         )
+        df_l2 = evalify.create_experiment(
+            self.embs, self.targets, metric="euclidean_distance_l2"
+        )
         self.assertGreater(df.euclidean_distance.max(), 0)
+        self.assertGreater(df_l2.euclidean_distance_l2.max(), 0)
 
     def test_create_experiment_cosine_similarity(self):
         """Test create_experiment with cosine_similarity"""
@@ -41,16 +47,45 @@ class TestEvalify(unittest.TestCase):
         )
         self.assertLessEqual(df.cosine_similarity.max(), 1)
 
+    def test_create_experiment_full_class_samples(self):
+        """Test create_experiment with return_embeddings"""
+        df = evalify.create_experiment(
+            self.embs,
+            self.targets,
+            same_class_samples="full",
+            different_class_samples=("full", "full"),
+        )
+        self.assertEqual(len(df), comb(self.nphotos, 2))
+
+    def test_create_experiment_custom_class_samples(self):
+        """Test create_experiment with custom same_class_samples and
+        different_class_samples
+        """
+        N, M = (2, 5)
+        same_class_samples = 3
+        df = evalify.create_experiment(
+            self.embs,
+            self.targets,
+            same_class_samples=2,
+            different_class_samples=(N, M),
+        )
+
+        self.assertLessEqual(
+            len(df),
+            (comb(same_class_samples, 2) * self.nclasses)
+            + (self.nclasses * (self.nclasses - 1)) * M * N,
+        )
+
     def test_create_experiment_return_embeddings(self):
         """Test create_experiment with return_embeddings"""
         df = evalify.create_experiment(self.embs, self.targets, return_embeddings=True)
-        self.assertLessEqual(len(df.img_a.iloc[0]), self.emb_size)
+        self.assertLessEqual(len(df.at[0, "img_a"]), self.emb_size)
 
     def test_create_experiment_errors(self):
         """Test create_experiment errors"""
         with self.assertRaisesRegex(
             ValueError,
-            '`same_class_samples` argument must be one of "full" or an integer',
+            "`same_class_samples` argument must be one of 'full' or an integer ",
         ):
             _ = evalify.create_experiment(
                 self.embs, self.targets, same_class_samples=54.4
@@ -58,7 +93,7 @@ class TestEvalify(unittest.TestCase):
 
         with self.assertRaisesRegex(
             ValueError,
-            '`different_class_samples` argument must be one of "full", "minimal"',
+            "`different_class_samples` argument must be one of 'full', 'minimal', ",
         ):
             _ = evalify.create_experiment(
                 self.embs, self.targets, different_class_samples="all"
